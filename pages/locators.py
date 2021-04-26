ff0ec4d7f6fdc6b59ac77df0ec67981f15b0d76b
Alex Zapolsky <mamboosjka@gmail.com>
2021-04-26 03:20:34 +0300

    Add locators for status messages

--- pages/locators.py
+++ pages/locators.py
@@ -1,6 +1,13 @@
 from selenium.webdriver.common.by import By
 
 
+class HeaderLocators():
+    BASKET_TOTAL_INFO = (By.CSS_SELECTOR, "div.basket-mini.pull-right")
+    VIEW_BASKET_BTN = (By.CSS_SELECTOR, "div.basket-mini.pull-right>span>a.btn")
+    STATUS_MSGS = (By.CSS_SELECTOR, "div#messages>div.alert")
+    STATUS_MSG_TEXT = (By.CSS_SELECTOR, "div.alertinner")
+    STATUS_INFO_MSG_TEXT = (By.CSS_SELECTOR, "div.alertinner>p:nth-child(1)")
+
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
@@ -16,3 +23,9 @@ class LoginPageLocators():
     PASS_FOR_REG_FLD = (By.CSS_SELECTOR, "imput#id_registration-password1")
     CONFIRM_PASS_FLD =(By.CSS_SELECTOR, "input#id_registration-password2")
     REGISTER_BTN = (By.CSS_SELECTOR, 'button.btn-lg.btn-primary[name="registration_submit"]')
+
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BTN = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    PROD_COST_INFO = (By.CSS_SELECTOR, "div.product_main p.price_color")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.col-sm-6.product_main>h1")
