f7fdc46b88971a658dde022d7167e90e2a4664d5
Alex Zapolsky <mamboosjka@gmail.com>
2021-04-26 03:25:39 +0300

    Add new methods to base_page for cover message verifing tests.

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,7 @@
 import math
+from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException
+from .locators import HeaderLocators
 
 class BasePage():
     def __init__(self, browser, url, timeout = 10):
@@ -30,3 +32,37 @@ class BasePage():
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
+    def normalise_cost(self,cost: str):
+        return float(cost[1:])
+
+    def get_total_cost_on_header(self):
+        durty_text = self.browser.find_element(*HeaderLocators.BASKET_TOTAL_INFO).text
+        return self.normalise_cost(durty_text.replace('Basket total:', '').replace('View basket', '').strip())        
+
+    def should_be_basket_total_increased_on(self, added_prod_coast=0.0, initial_price=0.0):
+        current_bskt_price = self.get_total_cost_on_header()
+        expected_bskt_price = initial_price+added_prod_coast
+        assert  (current_bskt_price == expected_bskt_price), f"Basket price: {current_bskt_price} is wrong!, Expext that it its: {(expected_bskt_price)}."
+
+    def get_text_of_status_message(self, message):
+        msg_classes = message.get_attribute("class").split(' ')
+        if "alert-info" in msg_classes:
+            return message.find_element(*HeaderLocators.STATUS_INFO_MSG_TEXT).text
+        return message.find_element(*HeaderLocators.STATUS_MSG_TEXT).text
+
+    def find_status_msg_with(self, expected_text: str):
+        messages = self.browser.find_elements(*HeaderLocators.STATUS_MSGS)
+        if not messages:
+            return False
+        for message in messages:
+            if self.get_text_of_status_message(message) == expected_text:
+                return True
+        #print(f"Current message text is: '{self.get_text_of_status_message(message)}'. Expected is {expected_text}")
+        return False
+
+    def should_be_status_msg_with(self, text: str):
+        assert self.find_status_msg_with(text), f"Cant find message with text: '{text}'."
+
+
+
