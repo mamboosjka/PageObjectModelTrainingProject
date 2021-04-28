3a0b588dcb7a282583061e8ccc7ab050df5d2be4
Alex Zapolsky <mamboosjka@gmail.com>
2021-04-28 17:50:40 +0300

    Add 3 new tests to test_proguct_page.py test suite (Without implementation).

--- test_product_page.py
+++ test_product_page.py
@@ -23,3 +23,20 @@ def test_guest_can_add_product_to_basket(browser, link):
     prod_page.should_be_status_msg_with(f"Your basket total is now £{prod_price + bascket_price}")
     prod_page.should_be_basket_total_increased_on(prod_price, bascket_price)
 
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    # Открываем страницу товара 
+    # Добавляем товар в корзину 
+    # Проверяем, что нет сообщения об успехе с помощью is_not_element_present
+    pass
+
+def test_guest_cant_see_success_message (browser):
+    # Открываем страницу товара 
+    # Проверяем, что нет сообщения об успехе с помощью is_not_element_present
+    pass
+
+def test_message_disappeared_after_adding_product_to_basket (browser):
+    # Открываем страницу товара
+    # Добавляем товар в корзину
+    # Проверяем, что нет сообщения об успехе с помощью is_disappeared
+    pass
+
